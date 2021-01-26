c6336733a98a1be8937451e25eeea4753eab9b16
alshaq <orlandoshaquillelewis85@gmail.com>
2021-01-26 01:45:44 -0500

    Complete Assignment

--- simple_data_management/Program.cs
+++ simple_data_management/Program.cs
@@ -13,7 +13,7 @@ namespace simple_data_management
             //list of string to manage the item added by users
             List<string> items = new List<string>();
             bool cont = true;
-            Console.WriteLine("Welcome to OL Data Management System.");
+            Console.WriteLine("Welcome to OL Data Management System.\n");
             string choice = "m";
 
             bool listEmpty = true;
@@ -27,46 +27,56 @@ namespace simple_data_management
                     choice = Console.ReadLine();
                 }
 
+                // conditional statements to select each option
                 if (choice.ToLower() == "c")
                 {
+                    // list is not empty; passing a reference of the list
                     listEmpty = CreateItem(ref items);
 
                 }
                 else if (choice.ToLower() == "r" && !listEmpty)
                 {
+                    //displays the items of the list; 
+                    //passing a reference of the list
                     ListItem(ref items);
 
                 }
                 else if (choice.ToLower() == "u" && !listEmpty)
                 {
+                    //updates item in the list
+                    //passing a reference of list
                     UpdateItem(ref items);
 
                 }
                 else if (choice.ToLower() == "d" && !listEmpty)
                 {
+                    //deletes an item from the list
+                    //passing a reference of list
                     listEmpty = DeleteItem(ref items);
-
                 }
-
                 else if (choice.ToLower() == "q")
-                {
+                {   
+                    //quits the program
                     cont = false;
                 }
                 else
                 {
-                    Console.WriteLine("Incorrect choice! Try again");
+                    //wrong user input
+                    Console.WriteLine("[>>>>Incorrect choice! Try again<<<<]");
                 }
-
-
-
             } while (cont == true);
 
         }
 
+        /*
+         * Displays the menu for the user
+         */
         static void DisplayMenu(bool listEmpty)
         {
             Console.WriteLine("Choose from the Menu:");
             Console.WriteLine("(Type c) Create an item.");
+            
+            //if list is empty, these options are not shown
             if (!listEmpty)
             {
                 Console.WriteLine("(Type r) List all items.");
@@ -77,51 +87,87 @@ namespace simple_data_management
             Console.WriteLine("(Type q) Quit :(");
 
         }
-
+        /*
+         * creates an item and adds it to the list
+         * takes a reference to a list 
+         * returns false because list is no longer emptyu
+         */
         static bool CreateItem(ref List<string> itemCache)
         {
             Console.Write("Enter an item >>> ");
             string item = Console.ReadLine();
 
             itemCache.Add(item);
+            
             return false;
         }
 
+        /*
+         * displays each item with a basic pagination
+         */
         static void ListItem(ref List<string> itemCache)
         {
             int cacheAmount = itemCache.Count();
-            int lastPage = 0;
-            int pageCount = 0;
-            int currentPage = 0;
-            int number = 0;
+            int displayCount = 0; //count each item iterated through
+            int currentPage = 0; // stores the current page
+            // total number of page
+            int totalPages = (cacheAmount / 5) + ((cacheAmount % 5 > 0)? 1 : 0) ;
 
+            Console.WriteLine("\nItems : ");
             for (int i = 0; i < cacheAmount; i++)
             {
+                if(displayCount == 5)
+                {
+                    Console.WriteLine($"Page {currentPage+1} 0f {totalPages} ");
+                    Console.WriteLine("(Type N/n) Next Page");
+                    
+                    if ((currentPage + 1) > 1)
+                    {
+                        Console.WriteLine("(Type P/p) Previous Page");
+                    }
+
+                    string cmd = Console.ReadLine();
+
+                    if(cmd.ToLower() == "n")
+                    {
+                        displayCount = 0;
+                        currentPage++;
+                    }else if (cmd.ToLower() == "p" && (currentPage + 1) > 1)
+                    {
+                        i = i - 10;
+                        displayCount = 0;
+                        currentPage--;
+                    }
+                    
+                }
+
                 Console.WriteLine((i + 1) + ") " + itemCache[i]);
-
-                if ((number + 1) == 5)
-                {
-                    currentPage++;
-                    number = 0;
-                }
-                else
-                {
-                    number++;
-                }
-
+                displayCount++;
+               
             }
 
-            Console.WriteLine("Pages: " + (currentPage + 1).ToString());
+            Console.WriteLine($"Page {currentPage+1} 0f {totalPages} ");
         }
 
+        /*
+         * Updates a single item
+         * takes a reference to a list
+         */
         static void UpdateItem(ref List<string> itemCache)
         {
-            int count = itemCache.Count();
-            Console.WriteLine("What item number you want to update >>>>");
             int index = 0;
-            if (!Int32.TryParse(Console.ReadLine(), out index) || index > count)
+
+            Console.WriteLine("What item number you want to update >>>>");
+
+            /*
+             * parsing the string into a int and if fail and checks
+             * if the index is too large the user will get an error message 
+             * Or if an int wasn't entered
+             */
+            if (!Int32.TryParse(Console.ReadLine(), out index) || index > itemCache.Count())
             {
-                Console.WriteLine("Bad index");
+                Console.WriteLine("[>>>>Incorrect Item Number<<<<]");
+
                 return;
             }
 
@@ -133,20 +179,30 @@ namespace simple_data_management
                 Console.WriteLine("New Item >>>>");
                 string update = Console.ReadLine();
 
-                itemCache[index - 1] = update;
+                itemCache[index - 1] = update; //updates item
 
-                Console.WriteLine("[Updated]");
+                Console.WriteLine("[Updated]\n");
             }
 
         }
+
+        /*
+        * Deletes a single item
+        * takes a reference to a list
+        */
         static bool DeleteItem(ref List<string> itemCache)
         {
-            int count = itemCache.Count();
-            Console.WriteLine("What item number you want to delete >>>>");
             int index = 0;
-            if (!Int32.TryParse(Console.ReadLine(), out index) || index > count)
+            Console.WriteLine("What item number you want to delete >>>>");
+
+            /*
+            * parsing the string into a int and if fail and checks
+            * if the index is too large the user will get an error message 
+            * Or if an int wasn't entered
+            */
+            if (!Int32.TryParse(Console.ReadLine(), out index) || index > itemCache.Count())
             {
-                Console.WriteLine("Bad index");
+                Console.WriteLine("[>>>>Incorrect Item Number<<<<]");
             }
             else
             {
@@ -155,8 +211,8 @@ namespace simple_data_management
 
                 if (Console.ReadLine().ToLower() == "yes")
                 {
-                    itemCache.RemoveAt(index - 1);
-                    Console.WriteLine("[Deleted]");
+                    itemCache.RemoveAt(index - 1); // deletes item
+                    Console.WriteLine("[Deleted]\n");
                 }
             }
             return itemCache.Count() == 0;
